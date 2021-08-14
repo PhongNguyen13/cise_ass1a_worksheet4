3fdb5a0844c07ead38a1eeb1870b9fb54c095317
PhongNguyen13 <81122421+PhongNguyen13@users.noreply.github.com>
2021-08-14 21:40:19 +1200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// eslint-disable-next-line no-unused-vars
+import Main from "./App";
+ 
+ReactDOM.render(
+  // eslint-disable-next-line react/jsx-no-undef
+  <App/>, 
+  document.getElementById("root")
+);
